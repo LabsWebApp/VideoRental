6c58059bfe811212a58218947668897d3bec0a6c
LabsWebApp <vl.skopin@gmail.com>
2024-05-20 19:28:16 +0300

    Case 1 - Test: Case1_ExpectedRentalDaysException2

--- Case1/VideoRental/Rental.cs
+++ Case1/VideoRental/Rental.cs
@@ -2,21 +2,30 @@
 
 namespace VideoRental;
 
-public class Rental(Movie movie, int days)
+public class Rental
 {
+    private readonly Movie _movie;
+    private int _days;
+
+    public Rental(Movie movie, int days)
+    {
+        _movie = movie;
+        Days = days;
+    }
+
     private int Days
     {
-        get => days;
+        get => _days;
         set
         {
             if (value < 0)
                 throw new RentalDaysException(value);
 
-            days = value;
+            _days = value;
         }
     }
 
-    public double CalculateDebt() => movie.RentalPrice * days;
+    public double CalculateDebt() => _movie.RentalPrice * _days;
 
     public void AddRentalDays(int d = 1) => Days += d;
     public void SubtractRentalDays(int d = 1) => Days -= d;
