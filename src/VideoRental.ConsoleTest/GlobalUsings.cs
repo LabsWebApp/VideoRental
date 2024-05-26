4bea0bbc9bf276eaf83dc9cfc1c956a570f931d0
LabsWebApp <vl.skopin@gmail.com>
2024-05-26 10:00:40 +0300

    Add - ConsoleTest

---
+++ src/VideoRental.ConsoleTest/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// Global using directives
+
+global using VideoRental;
+global using static System.Console;
---
+++ src/VideoRental.ConsoleTest/Program.cs
@@ -0,0 +1,13 @@
+﻿Customer customer = new(name: "Петя");
+customer.Rentals.Add(new Rental(Movie.RegularMovie("Movie1"), 6));
+customer.Rentals.Add(new Rental(Movie.NewReleaseMovie("Movie2"), 2));
+
+WriteLine($"Клиент с именем {customer.Name} должен нам {customer.CalculateDebit()}.");
+WriteLine(new string('*', 20));
+
+//WriteLine(ReportManager.GetSimpleReport());
+//WriteLine(new string('*', 20));
+//WriteLine(ReportManager.GetHtmlReport());
+
+// Delay
+ReadKey();
